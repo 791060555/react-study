72c6ac7209bcf213d0986d0f3f8c6b5db1d7062b
791060555 <791060555@qq.com>
2015-12-29 08:55:28 +0800

    提交

---
+++ public/scripts/test.js
@@ -0,0 +1,16 @@
+var FancyCheckbox = React.createClass({
+  render: function() {
+    var { checked, ...other } = this.props;
+    var fancyClass = checked ? 'FancyChecked' : 'FancyUnchecked';
+    // `other` contains { onClick: console.log } but not the checked property
+    return (
+        <div {...other} className={fancyClass} />
+    );
+  }
+});
+ReactDOM.render(
+    <FancyCheckbox checked={true} onClick={console.log.bind(console)}>
+      Hello world!
+    </FancyCheckbox>,
+    document.getElementById('example')
+);
